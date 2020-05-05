a107b53b7fd572d94ca51f6f8199cfa812eaa6cd
AspieCoder <lukebraithwaite7@gmail.com>
2020-05-05 16:18:45 +0100

    README added

--- 003.cpp
+++ 003.cpp
@@ -6,17 +6,14 @@ void primeFactors(long n)
 {
     std::vector<long> primefactors;
 
-    // Removing all the factors of two
     while (n % 2 == 0)
     {
         primefactors.push_back(long(2));
         n = n / 2;
     }
 
-    // Checking each odd number
     for (int i = 3; i <= sqrt(n); i = i + 2)
     {
-        // while i divides n, divide n by i
         while (n % i == 0)
         {
             primefactors.push_back(long(i));
@@ -24,7 +21,6 @@ void primeFactors(long n)
         }
     }
 
-    // As we have tried every number less than sqrt(n) the number must be prime
     if (n > 2)
     {
         primefactors.push_back(long(n));
--- 015.cpp
+++ 015.cpp
@@ -6,18 +6,13 @@ long binomialCoeff(int n, int k)
     long C[n + 1][k + 1];
     int i, j;
 
-    // Caculate value of Binomial Coefficient
-    // in bottom up manner
     for (i = 0; i <= n; i++)
     {
         for (j = 0; j <= std::min(i, k); j++)
         {
-            // Base Cases
             if (j == 0 || j == i)
                 C[i][j] = 1;
 
-            // Calculate value using previously
-            // stored values
             else
                 C[i][j] = C[i - 1][j - 1] +
                           C[i - 1][j];
